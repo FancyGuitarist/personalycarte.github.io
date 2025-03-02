e874f514f5dfa852df0d19f1f0eb6fd40c0d396f
FancyGuitarist <sifer15@ulaval.ca>
2025-03-02 17:24:34 -0500

    Removed root_path

--- app.py
+++ app.py
@@ -1,8 +1,7 @@
 from flask import Flask, render_template
-from pathlib import Path
 
-root_path = Path(__file__).parent
-app = Flask(__name__, template_folder=root_path)
+
+app = Flask(__name__, template_folder='.')
 
 
 @app.route('/')
