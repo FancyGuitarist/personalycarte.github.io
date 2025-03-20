ca923f1258c10d523bf1bd49d79509027ef56659
FancyGuitarist <sifer15@ulaval.ca>
2025-03-20 19:46:01 -0400

    feat: Retrieving coordinates
    Can now retrieve coordinates and convert data in the required format for itinerary.py functions.

--- flask_app.py
+++ flask_app.py
@@ -1,13 +1,18 @@
 from flask import Flask, render_template, flash, request, redirect, url_for
 
 
-app = Flask(__name__, template_folder='.')
+app = Flask(__name__, template_folder=".")
 
 time_keys = ["bus", "car", "bike", "walk"]
 toggle_keys = ["toggleCar", "toggleBus", "toggleBike", "toggleWalk"]
+coordinates_keys = ["coords_depart", "coords_arrival"]
 
 
 def init_times():
+    """
+    Initialize the times dictionary with default values.
+    :return:
+    """
     times = {}
     for key in time_keys:
         times[key] = "00:00"
@@ -15,6 +20,10 @@ def init_times():
 
 
 def get_times():
+    """
+    Get the times from the html form.
+    :return:
+    """
     times = {}
     for key in time_keys:
         times[key] = request.form.get(key)
@@ -22,6 +31,11 @@ def get_times():
 
 
 def init_toggles():
+    """
+    Initialize the transport toggles dictionary with default values.
+    Car is the only selected transport by default.
+    :return:
+    """
     toggles = {}
     for key in toggle_keys:
         if key == "toggleCar":
@@ -31,7 +45,11 @@ def init_toggles():
     return toggles
 
 
-def get_toggles():
+def get_toggles_dict():
+    """
+    Get the transport toggles from the html form.
+    :return:
+    """
     toggles = {}
     for key in toggle_keys:
         try:
@@ -41,6 +59,34 @@ def get_toggles():
     return toggles
 
 
+def convert_toggles_dict_to_list(toggles_dict):
+    toggles = []
+    for key in toggle_keys:
+        if toggles_dict[key] == "on":
+            toggles.append(key)
+    return toggles
+
+
+def get_coordinates_dict():
+    """
+    Get the coordinates from the html form.
+    :return:
+    """
+    coordinates = {}
+    for key in coordinates_keys:
+        try:
+            coordinates[key] = request.form.get(key)
+        except KeyError:
+            coordinates[key] = ""
+    return coordinates
+
+
+def convert_coordinates_dict_to_tuple(coordinates_dict):
+    for key in coordinates_keys:
+        coordinates_dict[key] = tuple(map(float, coordinates_dict[key].split(',')))
+    return coordinates_dict
+
+
 app.secret_key = "your_secret_key"  # Required for flashing messages
 
 
@@ -48,17 +94,21 @@ app.secret_key = "your_secret_key"  # Required for flashing messages
 def home():
     if request.method == "POST":
         times = get_times()
-        toggle_states = get_toggles()
+        toggle_states = get_toggles_dict()
 
         # Flash the submitted values (visible on the web page)
-        flash(f"Bus: {times['bus']}, Car: {times['car']}, Bike: {times['bike']}, Walk: {times['walk']}")
+        flash(
+            f"Bus: {times['bus']}, Car: {times['car']}, Bike: {times['bike']}, Walk: {times['walk']}"
+        )
         flash(f"Selected modes: {toggle_states}")
 
         return redirect(url_for("home"))  # Redirect to avoid form resubmission
     else:
         times = init_times()
         toggle_states = init_toggles()
-    return render_template("index.html", times=times, toggle_states=toggle_states)  # Load the HTML form
+    return render_template(
+        "index.html", times=times, toggle_states=toggle_states
+    )  # Load the HTML form
 
 
 @app.route("/map")
@@ -69,15 +119,22 @@ def map_page():
 @app.route("/submit_inputs", methods=["POST"])
 def submit_inputs():
     times = get_times()
-    toggle_states = get_toggles()
+    toggle_states = get_toggles_dict()
+    toggles_list = convert_toggles_dict_to_list(toggle_states)
+    coordinates = convert_coordinates_dict_to_tuple(get_coordinates_dict())
     print(times)
-    print(toggle_states)
+    print(toggles_list)
+    print(coordinates)
 
     # Flash the submitted values (visible on the web page)
-    flash(f"Bus: {times['bus']}, Car: {times['car']}, Bike: {times['bike']}, Walk: {times['walk']}")
-    # flash(f"Selected modes: {toggle_states}")
+    flash(
+        f"Bus: {times['bus']}, Car: {times['car']}, Bike: {times['bike']}, Walk: {times['walk']}"
+    )
+    flash(f"Selected modes: {toggle_states}")
 
-    return render_template("index.html", times=times, toggle_states=toggle_states)  # Load the HTML form
+    return render_template(
+        "index.html", times=times, toggle_states=toggle_states
+    )  # Load the HTML form
 
 
 if __name__ == "__main__":
--- itinerary.py
+++ itinerary.py
@@ -1,4 +1,5 @@
 import sys
+
 sys.argv.append(["--max-memory", "5G"])
 
 import datetime
@@ -6,26 +7,27 @@ import os
 import geopandas as gpd
 from r5py import TransportNetwork, DetailedItinerariesComputer, TransportMode
 
-class CustomItinerary():
+
+class CustomItinerary:
     def __init__(self):
         self.set_network()
 
     def get_transport_modes(self, transport_string):
         """
         Convert a list of transport modes as strings to a list of TransportMode objects
-        
+
         Parameters
         ----------
         transport_string : list of str
             List of transport modes as strings (e.g. ["bus", "walk", "bike", "car"])
-        
+
         Returns
         -------
         list of TransportMode
             List of transport modes as TransportMode objects
         """
         transport_modes = []
-        
+
         for t in transport_string:
             if t == "bus":
                 transport_modes.append(TransportMode.BUS)
@@ -37,21 +39,24 @@ class CustomItinerary():
                 transport_modes.append(TransportMode.CAR)
             else:
                 raise ValueError(f"Invalid transport mode: {t}")
-        
+
         return transport_modes
 
     def set_network(self):
         self.transport_network = TransportNetwork(
-            "Data/OSM/openstreetmap_capnat.pbf",
-            [
-                "Data/RTC/googletransit.zip"
-            ]
+            "Data/OSM/openstreetmap_capnat.pbf", ["Data/RTC/googletransit.zip"]
         )
 
-    def compute_itinerary(self, origin_tuple, destination_tuple, transport_str, departure = datetime.datetime.now()):
+    def compute_itinerary(
+        self,
+        origin_tuple,
+        destination_tuple,
+        transport_str,
+        departure=datetime.datetime.now(),
+    ):
         """
         Compute the itinerary between two points
-        
+
         Parameters
         ----------
         origin : tuple
@@ -68,16 +73,28 @@ class CustomItinerary():
         gpd.GeoDataFrame
             GeoDataFrame containing the itinerary
         """
-        origin = gpd.GeoDataFrame({"id": [0]},geometry=gpd.GeoSeries(gpd.points_from_xy([origin_tuple[0]], [origin_tuple[1]])), crs="EPSG:4326")
-        destination = gpd.GeoDataFrame({"id": [1]},geometry=gpd.GeoSeries(gpd.points_from_xy([destination_tuple[0]], [destination_tuple[1]])), crs="EPSG:4326")
+        origin = gpd.GeoDataFrame(
+            {"id": [0]},
+            geometry=gpd.GeoSeries(
+                gpd.points_from_xy([origin_tuple[0]], [origin_tuple[1]])
+            ),
+            crs="EPSG:4326",
+        )
+        destination = gpd.GeoDataFrame(
+            {"id": [1]},
+            geometry=gpd.GeoSeries(
+                gpd.points_from_xy([destination_tuple[0]], [destination_tuple[1]])
+            ),
+            crs="EPSG:4326",
+        )
         transport_modes = self.get_transport_modes(transport_str)
-        
+
         itinerary_computer = DetailedItinerariesComputer(
             self.transport_network,
             origins=origin,
             destinations=destination,
             departure=departure,
-            transport_modes=transport_modes
+            transport_modes=transport_modes,
         )
 
         itinerary = itinerary_computer.compute_travel_details()
@@ -89,12 +106,12 @@ class CustomItinerary():
     def get_best_itinerary(self, df):
         """
         Get the best itinerary from the computed itinerary
-        
+
         Parameters
         ----------
         df : gpd.GeoDataFrame
             GeoDataFrame containing the itinerary
-        
+
         Returns
         -------
         gpd.GeoDataFrame
@@ -117,7 +134,7 @@ class CustomItinerary():
     def save_itinerary(self, itinerary, origin, destination):
         """
         Save the itinerary to a GeoJSON file
-        
+
         Parameters
         ----------
         itinerary : gpd.GeoDataFrame
@@ -132,27 +149,27 @@ class CustomItinerary():
         itinerary["wait_time"] = itinerary["wait_time"].astype(str)
 
         with open("Result/itinerary.geojson", "w+") as f:
-            f.write('''var itinerary = {\
+            f.write("""var itinerary = {\
 "type": "FeatureCollection",\
 "name": "itinerary",\
 "crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },\
 "features": [{ "type": "Feature", "properties": { \
-"name": "origin" }, "geometry": { "type": "Point", "coordinates":''')
-            f.write(f'''[ {origin[0]}, {origin[1]} ]''')
+"name": "origin" }, "geometry": { "type": "Point", "coordinates":""")
+            f.write(f"""[ {origin[0]}, {origin[1]} ]""")
             f.write(" } },")
 
-            f.write('''{ "type": "Feature", "properties": { \
-"name": "destination" }, "geometry": { "type": "Point", "coordinates":''')
-            f.write(f'''[ {destination[0]}, {destination[1]} ]''')
+            f.write("""{ "type": "Feature", "properties": { \
+"name": "destination" }, "geometry": { "type": "Point", "coordinates":""")
+            f.write(f"""[ {destination[0]}, {destination[1]} ]""")
             f.write(" } },")
 
             for i, row in itinerary.iterrows():
-                f.write('''{ "type": "Feature", "properties": { ''')
+                f.write("""{ "type": "Feature", "properties": { """)
                 f.write(f'''"from_id": {row["from_id"]}, "to_id": {row["to_id"]}, "option": {row["option"]}, \
 "segment": {row["segment"]}, "transport_mode": "{row["transport_mode"]}", "departure_time": \
 "{row["departure_time"]}", "distance": {row["distance"]}, "travel_time": "{row["travel_time"]}", \
 "wait_time": "{row["wait_time"]}", "route": {('"' + row["route"] + '"') if row["route"] is not None else "null"},''')
-                f.write(''' }, "geometry": { "type": "LineString", "coordinates": ''')
+                f.write(""" }, "geometry": { "type": "LineString", "coordinates": """)
                 line = row["geometry"]
                 # Conversion en liste de listes
                 coords_list = [list(coord) for coord in line.coords]
@@ -164,9 +181,8 @@ class CustomItinerary():
         f.close()
 
     def __del__(self):
-        #os.remove("Result/itinerary.geojson")
+        # os.remove("Result/itinerary.geojson")
         pass
-
 
 
 if __name__ == "__main__":
